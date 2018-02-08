e42611b64313ea32e6f4de6f6b57365ff0df8d62
asliabir <abiruzzaman.molla@gmail.com>
2018-02-09 00:31:11 +0600

    Finisherd this project

--- inc/header.php
+++ inc/header.php
@@ -41,33 +41,8 @@ if(Session::get("login")==true){
 	<meta name="description" content="It is a website about education">
 	<meta name="keywords" content="blog,cms blog">
 	<meta name="author" content="Abir">
-	<link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.css">	
-	<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
-	<link rel="stylesheet" href="style.css"/>
-	<link rel="stylesheet" href="style.css">
-	<script src="js/jquery.js" type="text/javascript"></script>
-	<script src="js/jquery.nivo.slider.js" type="text/javascript"></script>
-<script type="text/javascript">
-$(window).load(function() {
-	$('#slider').nivoSlider({
-		effect:'random',
-		slices:10,
-		animSpeed:500,
-		pauseTime:5000,
-		startSlide:0, //Set starting Slide (0 index)
-		directionNav:false,
-		directionNavHide:false, //Only show on hover
-		controlNav:false, //1,2,3...
-		controlNavThumbs:false, //Use thumbnails for Control Nav
-		pauseOnHover:true, //Stop animation while hovering
-		manualAdvance:false, //Force manual transitions
-		captionOpacity:0.8, //Universal caption opacity
-		beforeChange: function(){},
-		afterChange: function(){},
-		slideshowEnd: function(){} //Triggers after all slides have been shown
-	});
-});
-</script>
+<?php include "scripts/css.php"; ?>
+<?php include "scripts/js.php"; ?>
 </head>
 <body>
 <div class="headersection templete clear">
---
+++ scripts/css.php
@@ -0,0 +1,4 @@
+	<link rel="stylesheet" href="font-awesome-4.5.0/css/font-awesome.css">	
+	<link rel="stylesheet" href="css/nivo-slider.css" type="text/css" media="screen" />
+	<link rel="stylesheet" href="style.css"/>
+	<link rel="stylesheet" href="style.css">
